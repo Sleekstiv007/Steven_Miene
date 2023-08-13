5673957e97563ac556f793c9db3141d45dc93ce6
Anna <tarerie00annie@gmail.com>
2023-08-13 19:34:28 +0100

    Updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiFillYoutube, AiFillFilePdf } from "react-icons/ai";
 
 const Work = () => {
   return (
@@ -15,34 +16,53 @@ const Work = () => {
             <p class="py-6">// Check out some of my recent work</p>
           </div>
           <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4 default_cursor_cs">
-            <div
-              class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs"
-              style={{
-                "background-image": "url()",
-              }}
-            >
-              <div class="opacity-0 group-hover:opacity-100 ">
+            <div class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs">
+              <div className="relative">
+                <AiFillYoutube
+                  className="flex justify-center text-red-700"
+                  size={55}
+                />
+              </div>
+              <div class="absolute opacity-0 group-hover:opacity-100 ">
                 <span class="text-2xl font bold text-white tracking-wider ">
-                  React JS Application
+                  YouTube - <span className="text-sm">Project Mavericks</span>
                 </span>
                 <div class="pt-8 text-center default_cursor_cs">
-                  <a href="" target="_blank">
-                    <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg default_pointer_cs">
-                      Code
+                  <a href="https://youtu.be/c2XNeki67Vs" target="_blank">
+                    <button class="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Watch
                     </button>
                   </a>
-                  <a href="" target="_blank">
+                </div>
+              </div>
+            </div>
+            <div class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs">
+              <div className="relative">
+                <AiFillFilePdf
+                  className="flex justify-center text-red-700"
+                  size={55}
+                />
+              </div>
+              <div class="absolute opacity-0 group-hover:opacity-100 ">
+                <span class="text-2xl font bold text-white tracking-wider ">
+                  PDF - <span className="text-sm">Project Mavericks</span>
+                </span>
+                <div class="pt-8 text-center default_cursor_cs">
+                  <a
+                    href="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:23db1d39-3ee2-3218-a119-4d268d74f638"
+                    target="_blank"
+                  >
                     <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Live
+                      Open
                     </button>
                   </a>
                 </div>
               </div>
             </div>
             <div
-              class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs"
+              class="shadow-lg shadow-[#040c16] group container  rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs"
               style={{
-                "background-image": "url()",
+                "background-image": "url(./net-image.png)",
               }}
             >
               <div class="opacity-0 group-hover:opacity-100 ">
@@ -50,36 +70,20 @@ const Work = () => {
                   React JS Application
                 </span>
                 <div class="pt-8 text-center default_cursor_cs">
-                  <a href="" target="_blank">
+                  <a
+                    href="https://github.com/Sleekstiv007/netflix-app"
+                    target="_blank"
+                    class="default_pointer_cs"
+                  >
                     <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg default_pointer_cs">
                       Code
                     </button>
                   </a>
-                  <a href="" target="_blank">
-                    <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                      Live
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div
-              class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs"
-              style={{
-                "background-image": "url()",
-              }}
-            >
-              <div class="opacity-0 group-hover:opacity-100 ">
-                <span class="text-2xl font bold text-white tracking-wider ">
-                  React JS Application
-                </span>
-                <div class="pt-8 text-center default_cursor_cs">
-                  <a href="" target="_blank" class="default_pointer_cs">
-                    <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg default_pointer_cs">
-                      Code
-                    </button>
-                  </a>
-                  <a href="" target="_blank" class="default_pointer_cs">
+                  <a
+                    href="https://netflix-app-alpha-lime.vercel.app/"
+                    target="_blank"
+                    class="default_pointer_cs"
+                  >
                     <button class="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg default_pointer_cs">
                       Live
                     </button>
@@ -87,7 +91,7 @@ const Work = () => {
                 </div>
               </div>
             </div>
-            <div
+            {/* <div
               class="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div default_cursor_cs"
               style={{
                 "background-image": "url()",
@@ -110,7 +114,7 @@ const Work = () => {
                   </a>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
