0e7f0a187a6bd098d647447e2a3b22121e5900bb
Anna <tarerie00annie@gmail.com>
2023-08-13 20:17:18 +0100

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -7,9 +8,7 @@ const Hero = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto  px-0 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, My Name is </p>
-        <h1
-          className=" text-4xl sm:text-7xl font-bold text-white"
-        >
+        <h1 className=" text-4xl sm:text-7xl font-bold text-white">
           Steven Miene
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
@@ -22,12 +21,14 @@ const Hero = () => {
           with teams to deliver engaging web experiences."
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowRight className="ml-3" />
-            </span>
-          </button>
+          <Link to="/work">
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
