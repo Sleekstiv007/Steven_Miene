045b033cf8e72e65b41206b56d1495049b989171
Anna <tarerie00annie@gmail.com>
2023-08-21 11:37:05 +0100

    Updated Pitch

--- src/components/Pitch.jsx
+++ src/components/Pitch.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Pitch = () => {
-  return <div>Pitch</div>;
+  return <div>
+    <a href="https://drive.google.com/drive/folders/15I5HRO4AAAL29iwb4jhmTu_5Yc-Jy5PL"></a>
+    <button>
+      View
+    </button>
+  </div>;
 };
 
 export default Pitch;
